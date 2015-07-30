971ec46b3f89112160c17361d83ba93094adfd22
remittor <remittor@gmail.com>
2015-07-30 17:46:06 +0300

    Change date format for prop "ro.com.android.dateformat"
    ro.com.android.dateformat=yyyy-MM-dd

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,17 @@
 add_lunch_combo cm_d10f-userdebug
 
+/bin/cp device/jsr/d10f/post_process_props_hook.py build/tools/
+chmod  0775 build/tools/post_process_props_hook.py
+
+cd build
+if grep -q "post_process_props_hook" tools/post_process_props.py
+then
+    echo '[build] post_process_props.py already patched';
+else
+    git am ../device/jsr/d10f/patches/build-post_process_props.patch || git am --abort
+fi
+croot
+
 cd frameworks/base
 if grep -q "ro.storage_list.override" services/java/com/android/server/MountService.java
 then
