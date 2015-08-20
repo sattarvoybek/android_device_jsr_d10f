5e728f2da6aa52cc213d4b6ee77dbd59943d6192
S-trace <S-trace@list.ru>
2015-08-21 02:10:28 +0300

    [SCRIPTS] Update for lolipoop (settings_headers.xml => dashboard_categories.xml)
    Change-Id: Ia10e5ae25e9ba7a12f31e305f35df778f366c515

--- update-overlay.sh
+++ update-overlay.sh
@@ -8,23 +8,23 @@ cd "$PATCHBASE"
 mkdir -p overlay/packages/apps/Settings/res/xml/
 
 xmlstarlet ed \
---insert '/preference-headers/header[@android:id="@+id/about_settings"]' \
---type elem -n header \
---insert '/preference-headers/header[not(@android:id)]' \
+--insert 'dashboard-categories/dashboard-category[@android:id="@+id/system_section"]/dashboard-tile[@android:id="@+id/about_settings"]' \
+--type elem -n dashboard-tile \
+--insert 'dashboard-categories/dashboard-category[@android:id="@+id/system_section"]/dashboard-tile[not(@android:id)]' \
 --type attr -n android:title -v "@string/device_settings" \
---insert '/preference-headers/header[not(@android:id)]' \
+--insert 'dashboard-categories/dashboard-category[@android:id="@+id/system_section"]/dashboard-tile[not(@android:id)]' \
 --type attr -n android:icon -v "@drawable/ic_settings_profiles" \
---subnode '/preference-headers/header[not(@android:id)]' \
+--subnode 'dashboard-categories/dashboard-category[@android:id="@+id/system_section"]/dashboard-tile[not(@android:id)]' \
 --type elem -n intent \
---insert '/preference-headers/header[not(@android:id)]/intent' \
+--insert 'dashboard-categories/dashboard-category[@android:id="@+id/system_section"]/dashboard-tile[not(@android:id)]/intent' \
 --type attr -n android:action -v "android.intent.action.MAIN" \
---insert '/preference-headers/header[not(@android:id)]/intent' \
+--insert 'dashboard-categories/dashboard-category[@android:id="@+id/system_section"]/dashboard-tile[not(@android:id)]/intent' \
 --type attr -n android:targetPackage -v "com.cyanogenmod.settings.device" \
---insert '/preference-headers/header[not(@android:id)]/intent' \
+--insert 'dashboard-categories/dashboard-category[@android:id="@+id/system_section"]/dashboard-tile[not(@android:id)]/intent' \
 --type attr -n android:targetClass -v "com.cyanogenmod.settings.device.SettingsActivity" \
---insert '/preference-headers/header[not(@android:id)]' \
+--insert 'dashboard-categories/dashboard-category[@android:id="@+id/system_section"]/dashboard-tile[not(@android:id)]' \
 --type attr -n android:id -v "@+id/device_settings" \
-../../../packages/apps/Settings/res/xml/settings_headers.xml > overlay/packages/apps/Settings/res/xml/settings_headers.xml
+../../../packages/apps/Settings/res/xml/dashboard_categories.xml > overlay/packages/apps/Settings/res/xml/dashboard_categories.xml
 
 xmlstarlet ed \
 -u '/PreferenceScreen/PreferenceScreen[@android:key="cm_updates"]/intent/@android:targetPackage' -v "eu.chainfire.opendelta" \
