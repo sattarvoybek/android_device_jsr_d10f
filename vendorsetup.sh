c9b18bc715fad917c8f4c600434e166f572aee67
S-trace <S-trace@list.ru>
2015-08-30 20:51:06 +0300

    [PATCHES] Reorganize patches
    Change-Id: Ib491cf477a69037274f1e4c5cbd24b5cfd266050

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,36 +1,28 @@
-s/bin/cp device/jsr/d10f/post_process_props_hook.py build/tools/
+/bin/cp device/jsr/d10f/post_process_props_hook.py build/tools/
 chmod  0775 build/tools/post_process_props_hook.py
-
-cd build
-if grep -q "post_process_props_hook" tools/post_process_props.py
-then
-    echo '[build] post_process_props.py already patched';
-else
-    git am ../device/jsr/d10f/patches/build-post_process_props.patch || git am --abort
-fi
-croot
-
-
-add_lunch_combo cm_d10f-eng
-add_lunch_combo cm_d10f-user
-add_lunch_combo cm_d10f-userdebug
-
-cd frameworks/base
-if grep -q "ro.storage_list.override" services/core/java/com/android/server/MountService.java
-then
-    echo '[storages] Frameworks/base already patched';
-else
-    git am ../../device/jsr/d10f/patches/frameworks-base-1.patch || git am --abort
-    git am ../../device/jsr/d10f/patches/frameworks-base-2.patch || git am --abort
-fi
-croot
 
 cd build
 if grep -q "UTC%z" tools/buildinfo.sh
 then
     echo '[build] buildinfo.sh already patched';
 else
-    git am ../device/jsr/d10f/patches/build-date-format-utc.patch || git am --abort
+    git am ../device/jsr/d10f/patches/build/0001-Change-date-format.patch || git am --abort
+fi
+if grep -q "post_process_props_hook" tools/post_process_props.py
+then
+    echo '[build] post_process_props.py already patched';
+else
+    git am ../device/jsr/d10f/patches/build/0002-Add-hook-functions-to-post_process_props.py.patch || git am --abort
+fi
+croot
+
+cd frameworks/base
+if grep -q "ro.storage_list.override" services/core/java/com/android/server/MountService.java
+then
+    echo '[storages] Frameworks/base already patched';
+else
+    git am ../../device/jsr/d10f/patches/frameworks/base/0001-MountService-allow-overriding-default-storage-list-w.patch || git am --abort
+    git am ../../device/jsr/d10f/patches/frameworks/base/0002-frameworks-hardcode-package-name-to-android.patch || git am --abort
 fi
 croot
 
@@ -39,7 +31,7 @@ if grep -q "tzdata2015f" libc/zoneinfo/tzdata
 then
     echo '[bionic] tzdata already patched';
 else
-    git am ../device/jsr/d10f/patches/bionic-tzdata.patch || git am --abort
+    git am ../device/jsr/d10f/patches/bionic/0001-PATCH-Update-tzdata-to-2015f.patch || git am --abort
 fi
 croot
 
@@ -53,3 +45,7 @@ croot
 #rm -rf out/target/product/d10f/obj/ETC/init.qcom.sdcard.rc_intermediates
 rm -rf out/target/product/d10f/obj/PACKAGING/target_files_intermediates
 rm -f out/target/product/d10f/system/build.prop
+
+add_lunch_combo cm_d10f-eng
+add_lunch_combo cm_d10f-user
+add_lunch_combo cm_d10f-userdebug
