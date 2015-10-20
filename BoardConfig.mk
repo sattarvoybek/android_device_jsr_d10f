20483e66125480a40b356612dac034ce334d5af3
S-trace <S-trace@list.ru>
2015-10-20 19:41:15 +0300

    [PROPS] Disable TARGET_UNIFIED_DEVICE to fix Device model=Unknown
    Change-Id: Ie307239c682a9bc0e64e8ee15ef098695253cb8d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -102,7 +102,6 @@ MAX_EGL_CACHE_SIZE := 2048*1024
 BOARD_HARDWARE_CLASS := device/jsr/d10f/cmhw/
 
 # Init
-TARGET_UNIFIED_DEVICE := true
 TARGET_INIT_VENDOR_LIB := libinit_msm
 TARGET_LIBINIT_DEFINES_FILE := device/jsr/d10f/init/init_d10f.c
 
