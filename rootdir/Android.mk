7c31c77e1c60a775e0fd748d9954b5937a867ca2
S-trace <S-trace@list.ru>
2015-10-21 19:35:09 +0300

    [FSTABS] Add missing fstab.d10f_sd package ...and remove deprecated fstab.int package
    Change-Id: Ia2c89f25dd61a8a8821ff4a7b4cc096534bb8bba

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -20,18 +20,10 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.int
+LOCAL_MODULE       := fstab.d10f_sd
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/fstab.int
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.sd
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/fstab.sd
+LOCAL_SRC_FILES    := etc/fstab.d10f_sd
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
