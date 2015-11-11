e0de690325eddf427e202c896771086ce1eecb2e
S-trace <S-trace@list.ru>
2015-11-11 23:25:46 +0300

    [PATCHES] Rework patches applying, fix patches for new vendorsetup.sh script
    Just add new patch to device/jsr/d10f/patches/repo/to/patch/ and
    it will be applied automatically next time You call . build/envsetup.sh
    
    Now it's need no specific check if every single patch applied anymore ^__^

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,97 +1,56 @@
-cd build
-if grep -q "UTC%z" tools/buildinfo.sh
-then
-    echo '[build] buildinfo.sh already patched';
-else
-    git am ../device/jsr/d10f/patches/build/0001-Change-date-format.patch || git am --abort
-fi
-if grep -q "post_process_props_hook" tools/post_process_props.py
-then
-    echo '[build] post_process_props.py already patched';
-else
-    git am ../device/jsr/d10f/patches/build/0002-Add-hook-functions-to-post_process_props.py.patch || git am --abort
-fi
-if grep -q -- "--block" core/Makefile
-then
-    git am ../device/jsr/d10f/patches/build/0003-Disable-block-zip-building-build-standard-file-based.patch || git am --abort
-else
-    echo '[build] post_process_props.py already patched';
-fi
-croot
+build_root=$(pwd) # vendorsetup.sh is sourced by build/envsetup.sh in root of android build tree. Hope that nobody can correctly source it not from root of android tree.
+echo "Applying patches"
+patches_path="$build_root/device/jsr/d10f/patches/"
+pushd "$patches_path" > /dev/null
+unset repos
+find -type f -name '*.patch'|cut -d / -f 2-|sort
+for patch in `find -type f -name '*.patch'|cut -d / -f 2-|sort`; do
+	# Strip patch title to get git commit title - git ignore [text] prefixes in beginning of patch title during git am
+	title=$(sed -rn "s/Subject: (\[[^]]+] *)*//p" "$patch")
+	absolute_patch_path="$patches_path/$patch"
+	# Supported both path/to/repo_with_underlines/file.patch and path_to_repo+with+underlines/file.patch (both leads to path/to/repo_with_underlines)
+	repo_to_patch="$(if dirname $patch|grep -q /; then dirname $patch; else dirname $patch |tr '_' '/'|tr '+' '_'; fi)"
 
-cd frameworks/base
-if grep -q "ro.storage_list.override" services/core/java/com/android/server/MountService.java
-then
-    echo '[storages] Frameworks/base already patched';
-else
-    git am ../../device/jsr/d10f/patches/frameworks/base/0001-MountService-allow-overriding-default-storage-list-w.patch || git am --abort
-    git am ../../device/jsr/d10f/patches/frameworks/base/0002-frameworks-hardcode-package-name-to-android.patch || git am --abort
-fi
-if grep -q "enabling torch via sysfs" services/core/java/com/android/server/TorchService.java
-then
-    echo '[Torch] Frameworks/base already patched';
-else
-    git am ../../device/jsr/d10f/patches/frameworks/base/0003-TORCH-Use-sysfs-interface-for-system-wide-torch-serv.patch || git am --abort
-fi
-if grep -q "/sys/class/power_supply/battery/charge_full_design" core/java/com/android/internal/os/PowerProfile.java
-then
-    echo '[Battery] Frameworks/base already patched';
-else
-    git am ../../device/jsr/d10f/patches/frameworks/base/0004-POWER-Read-battery-capacity-from-sysfs.patch || git am --abort
-fi
-if grep -q "DEFAULT_THRESHOLD_MAX_BYTES = 10" core/java/android/os/storage/StorageManager.java; then
-    echo '[storages] DEFAULT_THRESHOLD_MAX_BYTES in Frameworks/base already patched';
-else
-    git am ../../device/jsr/d10f/patches/frameworks/base/0005-STORAGES-Reduce-DEFAULT_THRESHOLD_MAX_BYTES-500MB-10.patch || git am --abort
-fi
-croot
+	echo -n "Is $repo_to_patch patched for '$title' ?.. "
+	pushd "$build_root/$repo_to_patch" > /dev/null
+	if (git log |fgrep -qm1 "$title" ); then
+		echo Yes
+	  commit_hash=$(git log --oneline |fgrep -m1 "$title"|cut -d ' ' -f 1)
+	  if [ q"$commit_hash" != "q" ]; then
+	  	  echo -n "Checking if patch $patch matches commit $commit_hash... "
+	  	  commit_id=$(git format-patch -1 --stdout $commit_hash |git patch-id|cut -d ' ' -f 1)
+	  	  patch_id=$(git patch-id < $absolute_patch_path|cut -d ' ' -f 1)
+#	  	  grep -oPz -- '(?s)-- ?\n[0-9.]+\n' $absolute_patch_path
+	  	  if [ $commit_id = $patch_id ]; then 
+	  	  	  echo 'Yes, it matches'
+	  	  else
+	  	  	  echo 'NO! MISMATCH!'
+	  	  	  sed '0,/^$/d' $absolute_patch_path|head -n -3  > /tmp/patch
+	  	  	  git show --stat $commit_hash -p --pretty=format:%b > /tmp/commit
+	  	  	  diff -u /tmp/patch /tmp/commit
+	  	  	  rm /tmp/patch /tmp/commit
+	  	  	  echo 'Resetting branch!'
+	  	  	  git checkout $commit_hash~1
+	  	  	  git am $absolute_patch_path || git am --abort
+          	  fi
+	else
+		echo "Unable to get commit hash for '$title'! Something went wrong!"
+		sleep 20
+	fi
+	else
+		echo No
+		echo "Trying to apply patch $(basename "$patch") to '$repo_to_patch'"
+		if ! git am $absolute_patch_path; then
+			echo "Failed, aborting git am"
+			git am --abort
+		fi
+	fi
+	popd > /dev/null
+done
+popd > /dev/null
+echo "Applying patches: done"
 
-cd kernel/jsr/msm8226
-if grep -q "VENUS_EXTRADATA_SIZE" include/media/msm_media_info.h
-then
-    echo '[kernel] include/media/msm_media_info.h already patched';
-else
-    git am ../../../device/jsr/d10f/patches/kernel/jsr/msm8226/0001-msm-vidc-Amend-calculation-of-buffer-sizes-in-VENUS_.patch || git am --abort
-    git am ../../../device/jsr/d10f/patches/kernel/jsr/msm8226/0002-msm-vidc-Expose-extradata-size-to-userspace.patch || git am --abort
-fi
-croot
-
-cd bionic
-if grep -q "tzdata2015g" libc/zoneinfo/tzdata
-then
-    echo '[bionic] tzdata already patched';
-else
-    git am ../device/jsr/d10f/patches/bionic/0001-TZDATA-Upgrade-tzdata-to-2015g.patch || git am --abort
-fi
-croot
-
-cd external/icu
-if grep -q "2015g" icu4c/source/data/misc/zoneinfo64.txt
-then
-    echo '[icu] zoneinfo already patched';
-else
-    git am ../../device/jsr/d10f/patches/external/icu/0001-TZDATA-Update-tzdata-to-2015g.patch || git am --abort
-fi
-croot
-
-cd packages/apps/Torch
-if grep -q android.hardware.ITorchService src/net/cactii/flash2/FlashDevice.java ; then
-    git am ../../../device/jsr/d10f/patches/packages/apps/Torch/0001-Revert-Torch-notify-TorchService-of-torch-state-when.patch || git am --abort
-    git am ../../../device/jsr/d10f/patches/packages/apps/Torch/0002-Revert-Make-torch-shutdown-by-camera-usage-work-prop.patch || git am --abort
-    git am ../../../device/jsr/d10f/patches/packages/apps/Torch/0003-Revert-Torch-signal-to-framework-TorchService-not-to.patch || git am --abort
-else
-    echo '[torch] src/net/cactii/flash2/FlashDevice.java already patched';
-fi
-croot
-
-cd system/vold
-if grep -q "exfat.ko" Exfat.cpp
-then
-    echo '[Vold] Exfat kernel driver support already patched';
-else
-    git am ../../device/jsr/d10f/patches/system/vold/0001-EXFAT-Use-kernel-driver-instead-of-FUSE-driver.patch || git am --abort
-fi
-croot
+echo "Updating overlay"
 
 sh device/jsr/d10f/update-overlay.sh
 rm -f out/target/product/d10f/system/build.prop
