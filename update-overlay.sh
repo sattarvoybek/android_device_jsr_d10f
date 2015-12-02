f5dc00c91e77deb955b2e48b48ffd1ec0cbef5b7
S-trace <S-trace@list.ru>
2015-12-02 20:53:47 +0300

    [OVERLAY] Update Settings overlay and update-overlay.sh for CM-13.0
    Change-Id: I97c74958c8163553737536a605148b8f80d4d663

--- update-overlay.sh
+++ update-overlay.sh
@@ -27,6 +27,7 @@ xmlstarlet ed \
 ../../../packages/apps/Settings/res/xml/dashboard_categories.xml > overlay/packages/apps/Settings/res/xml/dashboard_categories.xml
 
 xmlstarlet ed \
+-u '/PreferenceScreen/PreferenceScreen[@android:key="cm_updates"]/@android:title' -v "@string/system_update_settings_list_item_title" \
 -u '/PreferenceScreen/PreferenceScreen[@android:key="cm_updates"]/intent/@android:targetPackage' -v "eu.chainfire.opendelta" \
 -u '/PreferenceScreen/PreferenceScreen[@android:key="cm_updates"]/intent/@android:targetClass' -v "eu.chainfire.opendelta.MainActivity" \
 ../../../packages/apps/Settings/res/xml/device_info_settings.xml > overlay/packages/apps/Settings/res/xml/device_info_settings.xml
