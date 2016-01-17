4a994b0fd82e4a968b26ef9e8c6ace6553ff30e4
S-trace <S-trace@list.ru>
2016-01-17 12:42:03 +0300

    [TWRP] Remove obsolete fstab
    Change-Id: I303ccc48d78db7a579162214eaf8f648ad4151a8

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -28,14 +28,6 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := twrp.fstab
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/$(LOCAL_MODULE)
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.d10f_int
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
