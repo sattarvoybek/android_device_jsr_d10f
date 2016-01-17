1548b112ff6d7ea3eafba771f246c58fe0f17788
PlatonCraft <bmwgovno@gmail.com>
2016-01-17 11:28:22 +0300

    [SETTINGS] Add noise cancellation settings
    Change-Id: I1cea75929f04ce574d451f325445365806530777

--- JSR_Settings/java/com/cyanogenmod/settings/device/SettingsFragment.java
+++ JSR_Settings/java/com/cyanogenmod/settings/device/SettingsFragment.java
@@ -1,32 +1,28 @@
 package com.cyanogenmod.settings.device;
 
-import android.content.Intent;
 import android.content.ComponentName;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.SystemProperties;
-import android.preference.SwitchPreference;
 import android.preference.ListPreference;
-import android.preference.EditTextPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.preference.SwitchPreference;
 import android.provider.Settings;
-import android.widget.Toast;
 import android.util.Log;
+import android.widget.Toast;
 
-import java.io.BufferedReader;
 import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.IOException;
 import java.nio.charset.Charset;
-import java.util.Arrays;
 
 /**
  * Created by prodoomman on 19.02.15.
  */
-public class SettingsFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
+public class SettingsFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener, OnPreferenceClickListener {
 
-    private static final String TAG = "JSR";
+    private static final String TAG = "JSR_Settings";
     private static final String BTN_FUNC_APP = "btn_func_app";
     private static final String BTN_FUNC_APP2 = "btn_func_app2";
     private static final String PERSISTENT_PROPERTY_CONFIGURATION_NAME = "persist.storages.configuration";
@@ -34,10 +30,19 @@ public class SettingsFragment extends PreferenceFragment implements Preference.O
     private static final String STORAGES_CONFIGURATION_CLASSIC = "0" ;
     private static final String STORAGES_CONFIGURATION_INVERTED = "1" ;
     private static final String STORAGES_CONFIGURATION_DATAMEDIA = "2" ;
+    private static final String BTN_G_CAL = "btn_g_cal";
+    private static final String SWT_SPKR = "swt_spkr";
+    private static final String SWT_CALL = "swt_call";
+    private static final String SWT_VOICE_REC = "swt_voice_rec";
+    private static final String SWT_AUDIO_REC = "swt_audio_rec";
 
     EditTextPreferenceEx btn_func_app;
     EditTextPreferenceEx btn_func_app2;
     Preference btn_g_cal;
+    SwitchPreference swt_spkr;
+    SwitchPreference swt_call;
+    SwitchPreference swt_voice_rec;
+    SwitchPreference swt_audio_rec;
 
     private class SysfsValue {
         private String fileName;
@@ -88,17 +93,17 @@ public class SettingsFragment extends PreferenceFragment implements Preference.O
         boolean usbmscPresent = SystemProperties.getBoolean(USBMSC_PRESENT_PROPERTY_NAME, true);
         if (usbmscPresent) {
             main_storage=(ListPreference)findPreference("main_storage");
-	    main_storage.setEntries(new String[]{getString(R.string.storage_datamedia), getString(R.string.storage_usbmsc), getString(R.string.storage_sdcard)});
-	    main_storage.setEntryValues(new String[]{STORAGES_CONFIGURATION_DATAMEDIA, STORAGES_CONFIGURATION_CLASSIC, STORAGES_CONFIGURATION_INVERTED});
+            main_storage.setEntries(new String[]{getString(R.string.storage_datamedia), getString(R.string.storage_usbmsc), getString(R.string.storage_sdcard)});
+            main_storage.setEntryValues(new String[]{STORAGES_CONFIGURATION_DATAMEDIA, STORAGES_CONFIGURATION_CLASSIC, STORAGES_CONFIGURATION_INVERTED});
             configuration = SystemProperties.get(PERSISTENT_PROPERTY_CONFIGURATION_NAME, STORAGES_CONFIGURATION_CLASSIC);
         } else {
             main_storage=(ListPreference)findPreference("main_storage");
             main_storage.setEntries(new String[]{getString(R.string.storage_datamedia), getString(R.string.storage_sdcard)});
-	    main_storage.setEntryValues(new String[]{STORAGES_CONFIGURATION_DATAMEDIA, STORAGES_CONFIGURATION_INVERTED});
-	    configuration = SystemProperties.get(PERSISTENT_PROPERTY_CONFIGURATION_NAME, STORAGES_CONFIGURATION_DATAMEDIA);
+            main_storage.setEntryValues(new String[]{STORAGES_CONFIGURATION_DATAMEDIA, STORAGES_CONFIGURATION_INVERTED});
+            configuration = SystemProperties.get(PERSISTENT_PROPERTY_CONFIGURATION_NAME, STORAGES_CONFIGURATION_DATAMEDIA);
             if (configuration == STORAGES_CONFIGURATION_CLASSIC) {
-		configuration = STORAGES_CONFIGURATION_DATAMEDIA;
-	    }
+                configuration = STORAGES_CONFIGURATION_DATAMEDIA;
+            }
         }
 
         main_storage.setOnPreferenceChangeListener(this);
@@ -111,26 +116,21 @@ public class SettingsFragment extends PreferenceFragment implements Preference.O
         btn_func_app2 = (EditTextPreferenceEx)findPreference(BTN_FUNC_APP2);
         btn_func_app2.setOnPreferenceChangeListener(this);
 
-        btn_g_cal = (Preference)findPreference("btn_g_cal");
-        btn_g_cal.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener()
-        {
-            @Override
-            public boolean onPreferenceClick(Preference pref) {
-                if (pref.getKey().equals("btn_g_cal")) {
-                    try {
-                        Intent callGCal = new Intent();
-                        callGCal.setComponent(new ComponentName("com.qualcomm.sensors.qsensortest",
-                                "com.qualcomm.sensors.qsensortest.TabControl"));
-                        startActivity(callGCal);
-                    }
-                    catch(Exception ex) {
-                        Log.e("JSR_settings", "Failed to start GravityCalibrationActivity: " + ex);
-                        Toast.makeText(getActivity(), R.string.btn_g_cal_failure, Toast.LENGTH_LONG).show();
-                    }
-                }
-                return true;
-            }
-        });
+        btn_g_cal = (Preference)findPreference(BTN_G_CAL);
+        btn_g_cal.setOnPreferenceClickListener(this);
+
+        swt_spkr = (SwitchPreference)findPreference(SWT_SPKR);
+        swt_call = (SwitchPreference)findPreference(SWT_CALL);
+        swt_voice_rec = (SwitchPreference)findPreference(SWT_VOICE_REC);
+        swt_audio_rec = (SwitchPreference)findPreference(SWT_AUDIO_REC);
+        swt_spkr.setOnPreferenceChangeListener(this);
+        swt_call.setOnPreferenceChangeListener(this);
+        swt_voice_rec.setOnPreferenceChangeListener(this);
+        swt_audio_rec.setOnPreferenceChangeListener(this);
+        swt_spkr.setChecked(SystemProperties.getBoolean("persist.audio.fluence.speaker", false));
+        swt_call.setChecked(SystemProperties.getBoolean("persist.audio.fluence.voicecall", false));
+        swt_voice_rec.setChecked(SystemProperties.getBoolean("persist.audio.fluence.voicerec", false));
+        swt_audio_rec.setChecked(SystemProperties.getBoolean("persist.audio.fluence.audiorec", false));
     }
 
     @Override
@@ -145,6 +145,38 @@ public class SettingsFragment extends PreferenceFragment implements Preference.O
         }
         if (preference.getKey().equals(BTN_FUNC_APP2)) {
             Settings.System.putString(getActivity().getContentResolver(), preference.getKey(), (String)newValue);
+        }
+        if (preference.getKey().equals(SWT_SPKR)) {
+            SystemProperties.set("persist.audio.fluence.speaker", newValue.toString());
+            Toast.makeText(getActivity(), R.string.reboot_needed, Toast.LENGTH_LONG).show();
+        }
+        if (preference.getKey().equals(SWT_CALL)) {
+            SystemProperties.set("persist.audio.fluence.voicecall", newValue.toString());
+            Toast.makeText(getActivity(), R.string.reboot_needed, Toast.LENGTH_LONG).show();
+        }
+        if (preference.getKey().equals(SWT_VOICE_REC)) {
+            SystemProperties.set("persist.audio.fluence.voicerec", newValue.toString());
+            Toast.makeText(getActivity(), R.string.reboot_needed, Toast.LENGTH_LONG).show();
+        }
+        if (preference.getKey().equals(SWT_AUDIO_REC)) {
+            SystemProperties.set("persist.audio.fluence.audiorec", newValue.toString());
+            Toast.makeText(getActivity(), R.string.reboot_needed, Toast.LENGTH_LONG).show();
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onPreferenceClick(Preference preference) {
+        if (preference.getKey().equals(BTN_G_CAL)) {
+            try {
+                Intent callGCal = new Intent();
+                callGCal.setComponent(new ComponentName("com.qualcomm.sensors.qsensortest",
+                                                        "com.qualcomm.sensors.qsensortest.TabControl"));
+                startActivity(callGCal);
+            } catch (Exception ex) {
+                Log.e(TAG, "Failed to start GravityCalibrationActivity: " + ex);
+                Toast.makeText(getActivity(), R.string.btn_g_cal_failure, Toast.LENGTH_LONG).show();
+            }
         }
         return true;
     }
