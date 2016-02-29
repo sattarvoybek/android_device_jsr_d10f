281a84eabfe1fa128449c74fddb53d9441af61fa
S-trace <S-trace@list.ru>
2016-02-29 22:51:52 +0300

    [TWRP] Enable ntfs-3g support
    Change-Id: I6e543bde0671ac6490cbc6a7c5efadfdb16c66de

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -144,6 +144,7 @@ ifeq ($(RECOVERY_VARIANT), twrp)
 	TW_SECONDARY_BRIGHTNESS_PATH := /sys/class/leds/button-backlight/brightness
 	TW_THEME := portrait_hdpi
 	TW_NO_EXFAT_FUSE := true
+        TW_INCLUDE_NTFS_3G := true
 	PRODUCT_EXTRA_RECOVERY_KEYS += \
 		vendor/jsr/security/cm-12.1/releasekey \
 		vendor/jsr/security/cm-13.0/releasekey
