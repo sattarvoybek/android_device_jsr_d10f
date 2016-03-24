LineageOS/android_device_xiaomi_armani

Add camera headers

BoardConfig.mk
@@ -81,6 +81,9 @@ MAX_EGL_CACHE_SIZE := 2048*1024
 # Hardware tunables
 BOARD_HARDWARE_CLASS := device/xiaomi/armani/cmhw/
 
+# Headers
+TARGET_SPECIFIC_HEADER_PATH := device/xiaomi/armani/include
+
 # Init
 TARGET_UNIFIED_DEVICE := true
 TARGET_INIT_VENDOR_LIB := libinit_msm

sattarvoybek/android_device_jsr_d10f

BoardConfig.mk
@@ -90,6 +90,9 @@ MAX_EGL_CACHE_SIZE := 2048*1024
 # Hardware tunables
 BOARD_HARDWARE_CLASS := device/jsr/d10f/cmhw/
 
+# Headers
+TARGET_SPECIFIC_HEADER_PATH := device/jsr/d10f/include
+
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_msm
 TARGET_LIBINIT_DEFINES_FILE := device/jsr/d10f/init/init_d10f.cpp
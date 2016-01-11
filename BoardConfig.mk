a4830c6855e5bb2e4c7c5c065a85dbcafd3de177
fefifofum <jorsellis@gmail.com>
2016-01-11 01:57:31 +0100

    Add camera headers

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -90,6 +90,9 @@ MAX_EGL_CACHE_SIZE := 2048*1024
 # Hardware tunables
 BOARD_HARDWARE_CLASS := device/jsr/d10f/cmhw/
 
+# Headers
+TARGET_SPECIFIC_HEADER_PATH := device/jsr/d10f/include
+
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_msm
 TARGET_LIBINIT_DEFINES_FILE := device/jsr/d10f/init/init_d10f.cpp
