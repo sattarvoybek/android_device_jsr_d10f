f034c132c7c01b67800a6421617f10d58745c21a
remittor <remittor@gmail.com>
2016-04-27 15:11:28 +0300

    init: Create storage_list.xml on init
    New path: /data/system/storage_list.xml

--- init/init_d10f.c
+++ init/init_d10f.c
@@ -15,17 +15,73 @@
 
 #include "init_msm.h"
 
-#define PERSISTENT_PROPERTY_DIR  "/data/property"
+#define SWAP_PCH(_a_,_b_) { const char * x = (_a_); (_a_) = (_b_); (_b_) = x; }
+#define SWAP_INT(_a_,_b_) { int x = (_a_); (_a_) = (_b_); (_b_) = x; }
 
-void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char * board_type)
+#define PERSISTENT_PROPERTY_DIR  "/data/property"
+#define PERSISTENT_PROPERTY_PLANNED_SWAP    "persist.storages.planned_swap"
+#define PERSISTENT_PROPERTY_SWAPPED         "persist.storages.swapped"
+
+static const char * prop_skip_list[] = {
+	PERSISTENT_PROPERTY_PLANNED_SWAP,
+};
+
+
+#define FILE_STORAGE_LIST  "/data/system/storage_list.xml"
+
+typedef struct storage_item {
+	char * mountPoint;
+	char * storageDescription;
+	int primary;
+	int removable;
+	int emulated;
+	int mtpReserve;
+	int allowMassStorage;
+	int maxFileSize;
+};
+
+static struct storage_item storage_list[] = {
+	{
+		.mountPoint = "/storage/sdcard0",
+		.storageDescription = "@string/storage_internal",
+		.primary = 1,
+		.removable = 0,
+		.allowMassStorage = 0,
+	},
+	{
+		.mountPoint = "/storage/sdcard1",
+		.storageDescription = "@string/storage_sd_card",
+		.primary = 0,
+		.removable = 1,
+		.allowMassStorage = 1,
+	},
+	{
+		.mountPoint = "/storage/usbdisk",
+		.storageDescription = "@string/storage_usb",
+		.primary = 0,
+		.removable = 1,
+		.allowMassStorage = 0,
+	},
+};
+
+
+int check_skip_list(const char * prop_name)
 {
-	UNUSED(msm_id);
-	UNUSED(msm_ver);
-	UNUSED(board_type);
-	
+	size_t i;
+	size_t len = strlen(prop_name);
+	for (i = 0; i < ARRAY_SIZE(prop_skip_list); i++) {
+		if (prop_skip_list[i] && strlen(prop_skip_list[i]) == len) {
+			if (strcmp(prop_skip_list[i], prop_name) == 0) {
+				return 1; 
+			}
+		}
+	}
+	return 0;
+}
+
+int init_prop_list(void)
+{
 	char value[PROP_VALUE_MAX];
-	int rc;
-	
 	DIR * dir = opendir(PERSISTENT_PROPERTY_DIR);
 	int dir_fd;
 	struct dirent * entry;
@@ -36,7 +92,7 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char * boa
 		dir_fd = dirfd(dir);
 		while ((entry = readdir(dir)) != NULL) {
 			// we need to read this properties before load_persistent_properties()
-			if (strncmp("persist.storages.planned_swap", entry->d_name, strlen("persist.storages.planned_swap")))
+			if (check_skip_list(entry->d_name))
 				continue;
 #if HAVE_DIRENT_D_TYPE
 			if (entry->d_type != DT_REG)
@@ -80,20 +136,59 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char * boa
 	} else {
 		ERROR("Unable to open persistent property directory %s errno: %d\n", PERSISTENT_PROPERTY_DIR, errno);
 	}
-	
+	return 0;
+}
+
+int create_storage_list(void)
+{
+	size_t i;
+	FILE * f = fopen(FILE_STORAGE_LIST, "w+");
+	if (f == NULL) {
+		ERROR("can not open '%s', err: %s\n", FILE_STORAGE_LIST, strerror(errno));
+		return -1;
+	}
+	fprintf(f, "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n");
+	fprintf(f, "<StorageList>\n");
+	for (i = 0; i < ARRAY_SIZE(storage_list); i++) {
+		struct storage_item * s = &storage_list[i];
+		fprintf(f, "  <storage mountPoint=\"%s\" \n", s->mountPoint);
+		fprintf(f, "    storageDescription=\"%s\" \n", s->storageDescription);
+		fprintf(f, "    primary=\"%s\" \n", s->primary ? "true" : "false");
+		fprintf(f, "    removable=\"%s\" \n", s->removable ? "true" : "false");
+		fprintf(f, "    emulated=\"%s\" \n", s->emulated ? "true" : "false");
+		fprintf(f, "    allowMassStorage=\"%s\" \n", s->allowMassStorage ? "true" : "false");
+		fprintf(f, "  /> \n");
+	}
+	fprintf(f, "</StorageList>\n");
+	fclose(f);
+	return 0;
+}
+
+void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char * board_type)
+{
+	UNUSED(msm_id);
+	UNUSED(msm_ver);
+	UNUSED(board_type);
+
+	int rc;
+	char value[PROP_VALUE_MAX];
+
+	init_prop_list();
+
 	mount("rootfs", "/", "rootfs", MS_REMOUNT|0, NULL);
-	
-	rc = property_get("persist.storages.planned_swap", value);
+
+	rc = property_get(PERSISTENT_PROPERTY_PLANNED_SWAP, value);
 	if (rc && atoi(value)) {
-		// if swapped
-		property_set("persist.storages.swapped", "1");
-		property_set("ro.storage_list.override", "storage_list_swapped");
+		SWAP_PCH(storage_list[0].mountPoint, storage_list[1].mountPoint);
+		SWAP_INT(storage_list[0].primary, storage_list[1].primary);
+		property_set(PERSISTENT_PROPERTY_SWAPPED, "1");
 		symlink("/fstab.sd", "/fstab.qcom");
 	} else {
-		// if not swapped
-		property_set("persist.storages.swapped", "0");
+		property_set(PERSISTENT_PROPERTY_SWAPPED, "0");
 		symlink("/fstab.int", "/fstab.qcom");
 	}
 
 	mount("rootfs", "/", "rootfs", MS_REMOUNT|MS_RDONLY, NULL);
+
+	create_storage_list();
 }
