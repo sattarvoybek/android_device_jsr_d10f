3900e0e9e41ef2fcb60f55f4015981c315709217
remittor <remittor@gmail.com>
2016-05-18 18:32:44 +0300

    Add support QCOM proprietary

--- device.mk
+++ device.mk
@@ -124,16 +124,24 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/rootdir/etc/init.qcom.fm.sh:system/etc/init.qcom.fm.sh
 
 # GPS
-PRODUCT_PACKAGES += \
-    gps.msm8226 \
-    libgps.utils \
-    libloc_adapter \
-    libloc_eng
+PRODUCT_PACKAGES += gps.msm8226
+PRODUCT_PACKAGES += libgps.utils
+PRODUCT_PACKAGES += libloc_adapter
+PRODUCT_PACKAGES += libloc_eng
+ifeq ($(QCPATH),)
+PRODUCT_PACKAGES += gps.conf
+PRODUCT_PACKAGES += sap.conf
+else
+PRODUCT_PACKAGES += libloc_core
+PRODUCT_PACKAGES += libloc_api_v02
+endif
 
 PRODUCT_PROPERTY_OVERRIDES += ro.gps.agps_provider=1
 PRODUCT_PROPERTY_OVERRIDES += persist.gps.qc_nlp_in_use=0
 
+ifeq ($(QCPATH),)
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf
+endif
 
 # IRSC
 PRODUCT_COPY_FILES += \
