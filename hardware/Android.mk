d4e3b1edb998b51530c79712faaa7d760eefab26
remittor <remittor@gmail.com>
2016-06-07 18:04:32 +0300

    Revert "Add QCamera2 HAL"
    This reverts commit 190d22774a26141df1250e5bcabe2bce14b7375c.

--- hardware/Android.mk
+++ hardware/Android.mk
@@ -3,10 +3,6 @@ TARGET_HARDWARE_PATH := $(call my-dir)
 ifneq ($(QCPATH),)
 ifeq ($(TARGET_DEVICE),d10f)
 
-ifeq ($(strip $(USE_DEVICE_SPECIFIC_CAMERA)),true)
-include $(TARGET_HARDWARE_PATH)/camera/Android.mk
-endif
-
 ifeq ($(TARGET_PROVIDES_GPS_LOC_API),true)
 include $(TARGET_HARDWARE_PATH)/gps/Android.mk
 endif
